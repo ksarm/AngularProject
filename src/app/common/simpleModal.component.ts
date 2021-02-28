fcc483ef3e9e88053c8239d0e3ed7e849249eead
Konstantinos Sarmidis <ksarmidis@gmail.com>
2021-02-28 16:10:49 +0200

    (fix) close modal on body click and selection

--- src/app/common/simpleModal.component.ts
+++ src/app/common/simpleModal.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input } from '@angular/core'
+import { Component, ElementRef, Inject, Input, ViewChild } from '@angular/core'
+import { JQ_TOKEN } from './jQuery.service';
 
 @Component ({
     selector:'simple-modal',
     template: `
-    <div id="{{elementId}}" class="modal fade" tabindex=-1>
+    <div id="{{elementId}}" #modalcontainer class="modal fade" tabindex=-1>
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
@@ -11,7 +12,7 @@ import { Component, Input } from '@angular/core'
                     <span>&times;</span></button>
                     <h4 class="modal-title">{{title}}</h4>
                 </div>
-                <div class="modal.body">
+                <div class="modal.body" (click)="closeModal()">
                     <ng-content></ng-content>
                 </div>
             </div>
@@ -26,4 +27,14 @@ import { Component, Input } from '@angular/core'
 export class SimpleModalComponent {
     @Input() title:string;
     @Input() elementId:string;
+    @Input() closeOnBodyClick: string;
+    @ViewChild('modalcontainer') containerEL: ElementRef;
+
+    constructor(@Inject(JQ_TOKEN) private $:any) {}
+
+    closeModal() {
+        if(this.closeOnBodyClick.toLocaleLowerCase() === "true"){
+            this.$(this.containerEL.nativeElement).modal('hide');
+        }
+    }
 }
